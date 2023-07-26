f18c48477a6ade468b2d0ca6f8fde7f4c92670a5
Gustavo Carreño <carrenogustavo@gmail.com>
2023-07-26 09:11:39 -0600

    Initial Commit

---
+++ Platpp/PlatppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatppApp.swift
+//  Platpp
+//
+//  Created by Gustavo on 26/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PlatppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
